008268ec7a3a04590f3fb9f615079d8f331d49aa
mrmki100 <30376839+mrmki100@users.noreply.github.com>
2026-06-21 20:05:38 +0330

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -56,7 +56,7 @@ subprojects {
     val forceUpgrade: Project.() -> Unit = {
         plugins.withId("com.android.library") {
             extensions.configure<com.android.build.api.dsl.LibraryExtension> {
-                compileSdk = 34
+                compileSdk = 36
                 
                 // ONLY fix Isar, leave file_picker alone!
                 if (project.name == "isar_flutter_libs" && namespace == null) {
@@ -66,7 +66,7 @@ subprojects {
         }
         plugins.withId("com.android.application") {
             extensions.configure<com.android.build.api.dsl.ApplicationExtension> {
-                compileSdk = 34
+                compileSdk = 36
             }
         }
     }
